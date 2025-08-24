394ada291e79ba66cd5cbf9bd59610a5bb1c189d
mariajosehincapie18 <mjhincapie18@gmail.com>
2025-08-23 22:06:50 -0500

    calculos

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pro = GestorProductos()
 pro.precargar_productos_desde_txt("productos.txt")
 pro.mostrar_productos()
 op = Operaciones()
-resultado =op.busqueda_binaria_por_nombre(pro.productos,"taza")
+resultado =op.busqueda_binaria_por_nombre(pro.productos,"Taza cafetera")
 if resultado:
     print("\n🔎 Resultados encontrados:")
     print(resultado) 
@@ -15,4 +15,22 @@ else:
     print("\n❌ No se encontraron coincidencias.")
 
 
+calculo_prodctos = op.calculo_total_de_precios(pro.productos)
+print(calculo_prodctos)
 
+calculo_promedio = op.calculo_promedio_por_categoria(pro.productos, "souvenir")
+print(calculo_promedio)
+
+
+filtro_rango = op.busqueda_por_rango_de_precios(pro.productos,120,200)
+print(filtro_rango)
+
+
+reco = op.recomendaciones(pro.productos,"pin",)
+print(reco)
+
+
+seleccionado = op.busqueda_binaria_por_nombre(pro.productos, "pin")
+if seleccionado:
+    sugerencia = op.recomendaciones(pro.productos,seleccionado)
+    print(sugerencia)
--- operaciones.py
+++ operaciones.py
@@ -28,6 +28,65 @@ class Operaciones():
             
         else:
             return self.busqueda_binaria_por_nombre(productos,objetivo, medio + 1, fin)
+        
+    def calculo_total_de_precios(self, productos, index = 0, total_productos =0):
+        if productos is None:
+            return
+        if index == len(productos):
+            return total_productos
+        
+        producto_atual = productos[index].precio
+        total_productos = producto_atual + total_productos
+        return self.calculo_total_de_precios(productos,index +1, total_productos)
+    
+    def calculo_promedio_por_categoria(self, productos,categoria,  index = 0, cont = 0, suma_productos = 0):
+        if productos is None or len(productos) == 0:
+            return 0
+        
+        if index == len(productos):
+            return (suma_productos//cont)
+        
+        if productos[index].categoria.lower() == categoria.lower():
+            cont = +1
+            suma_productos = productos[index].precio + suma_productos
+        
+        return self.calculo_promedio_por_categoria(productos,categoria, index +1, cont, suma_productos)
+    
+
+    def busqueda_por_rango_de_precios(self, productos,minimo, maximo , index=0, resultado = None):
+        if resultado is None:
+            resultado = []
+        
+        if index == len(productos):
+            return resultado
+
+        if minimo <=  productos[index].precio <= maximo:
+            resultado.append(productos[index])
+        
+        return self.busqueda_por_rango_de_precios( productos, minimo,maximo, index +1, resultado)
+    
+
+    def recomendaciones(self, productos, producto_seleccionado, index =0, recomendaciones = None):
+        if recomendaciones is None:
+            recomendaciones = []
+
+        if index == len(productos):
+            return recomendaciones
+        
+        if productos[index].categoria.lower() == producto_seleccionado.categoria.lower() and productos[index] != producto_seleccionado:
+            recomendaciones.append(productos[index])
+
+        return self.recomendaciones(productos,producto_seleccionado, index +1, recomendaciones)
+
+        
+        
+        
+
+
+        
+         
+        
+
             
 
 
